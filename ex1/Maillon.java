0df9ebe1fd1a5695d79481e6b3a84164aa49911d
Cheikh Gaye <108100464+Kheush98@users.noreply.github.com>
2024-04-16 14:45:34 +0200

    Update Maillon.java

--- ex1/Maillon.java
+++ ex1/Maillon.java
@@ -5,7 +5,7 @@ class Maillon {
     char mot; 
     Maillon suiv; 
     // Constructeur pour créer un nouveau maillon avec un mot donné
-    Maillon(char mot) {
+   public Maillon(char mot) {
         this.mot = mot;
         this.suiv = null;
     }
